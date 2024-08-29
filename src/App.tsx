495836bd6c29dff6f64f401c96cf8a3fd0216f0c
sethuvicky <you@example.com>
2024-08-29 18:42:43 +0530

    Initial project setup with components and test files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoanCalculator from './pages/LoanCalculator';
+import './styles/globals.css';
+
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <LoanCalculator />
+    </div>
+  );
+};
+
+export default App;
